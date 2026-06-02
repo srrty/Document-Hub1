ff8164c86838ab33c07aa8e3daf69a3e2f3c7f49
srrty <53166845+srrty@users.noreply.github.com>
2026-06-02 19:37:11 +0900

    Change Google client ID in app.js
    Updated Google client ID for OAuth2 login.

--- public/app.js
+++ public/app.js
@@ -280,7 +280,8 @@ const btnGoogleLogin = document.getElementById('btn-google-login');
 if (btnGoogleLogin) {
     btnGoogleLogin.addEventListener('click', () => {
         const client = google.accounts.oauth2.initTokenClient({
-            client_id: '105861234289-sjr668m9k60b2401f8g366mrc760f35p.apps.googleusercontent.com',
+            // public/app.js 파일의 283번째 줄을 이 코드로 정확하게 교체하세요!
+            client_id: '105861234289-sjr668m9k6ob2401f8g366mrc760f35p.apps.googleusercontent.com',
             scope: 'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile',
             callback: async (response) => {
                 if (response.access_token) {
